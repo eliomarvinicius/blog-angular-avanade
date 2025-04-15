b17116a3b7873351653eaba57a79c770a780f5b8
Eliomar Vinicius <138163810+eliomarvinicius@users.noreply.github.com>
2025-04-15 17:33:20 -0300

    First commit
    Criação e publicação do repositorio

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MenuTitleComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-blog';
+}
---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+
+  @Input()
+  photoCover:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  cardDescription:string=""
+
+  constructor(){}
+
+  ngOnInit(): void {}
+}
